24b776eabf6d71b1492c6c39f88b9cd2c6fc0e63
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-01-18 07:59:52 +0530

    half smart contract done

--- contracts/lfgho.sol
+++ contracts/lfgho.sol
@@ -1,71 +1,107 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
-import {IGhoToken} from "https://github.com/aave/gho-core/blob/main/src/contracts/gho/interfaces/IGhoToken.sol";
+import {IGhoToken} from "./IGhoToken.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract lfgho is IGhoToken, ERC20 , AccessControl{
+contract lfgho is IGhoToken, ERC20, AccessControl {
     address public admin;
-    string value1="This function is not in use";
-    bytes32 value2="This function is not in use";
-    uint256 a=1;
-    uint256 b=1;
-    // address[2] arr=[address(0x0000000000000000000000000000000000000000),address(0x0000000000000000000000000000000000000000)];
-    address[] arr; 
+    uint256 num;
+
+    struct MyFacilitator {
+        uint256 bucketCapacity;
+        uint256 bucketLevel;
+        string label;
+        uint256 count;
+        address addrs;
+    }
+    mapping(address => MyFacilitator) internal myFacilitators;
 
     constructor() ERC20("Gho Token", "GHO") {
         admin = msg.sender;
     }
 
+    function mint(address account, uint256 amount) external {
+        require(amount > 0, "INVALID_MINT_AMOUNT");
+        myFacilitators[account].bucketCapacity = 1;
+        require(
+            amount <= myFacilitators[account].bucketCapacity,
+            "Amount generating is exceeding the capacity, shoud be only 1"
+        );
+        myFacilitators[account].bucketLevel = amount;
+        myFacilitators[account].addrs = account;
+        myFacilitators[account].label = "DAO MEMBER";
+        _mint(account, 1);
+    }
+
+    function vouch(address account) public {
+        require(
+            myFacilitators[msg.sender].bucketLevel == 1,
+            "You are not a valid facilitator"
+        );
+        myFacilitators[account].count++;
+    }
+
+    function burn(uint256 amount) external {
+        value1 = "This function is not in use";
+    }
+
+    function getDetails(
+        address account
+    ) public view returns (MyFacilitator memory) {
+        return myFacilitators[account];
+    }
+
+    ///////////////// USELESS FUNCTION //////////////////////////////  NO NEED TO FOCUS ON THESE /////////////////////////////////////
+
+    string value1 = "This function is not in use";
+    bytes32 value2 = "This function is not in use";
+    uint256 a = 1;
+    uint256 b = 1;
+    address[] arr;
+
     mapping(address => Facilitator) internal _facilitators;
 
-    function mint(address account, uint256 amount) external  {
-        require(amount > 0, "INVALID_MINT_AMOUNT");
-
-      
-
-        _mint(account, amount);
-    } 
-
-    function addFacilitator(  address facilitatorAddress,string calldata facilitatorLabel,uint128 bucketCapacity) external  {
-        value1="This function is not in use";
+    function addFacilitator(
+        address facilitatorAddress,
+        string calldata facilitatorLabel,
+        uint128 bucketCapacity
+    ) external {
+        value1 = "This function is not in use";
     }
 
-     function FACILITATOR_MANAGER_ROLE() external pure returns (bytes32){
+    function FACILITATOR_MANAGER_ROLE() external pure returns (bytes32) {
         return "This function is not in use";
-     }
-
-
-  function BUCKET_MANAGER_ROLE() external pure returns (bytes32){
-            return "This function is not in use";
-  }
-
-    function burn(uint256 amount) external{
-        value1="This function is not in use";
     }
 
-      function removeFacilitator(address facilitatorAddress) external{
-                value1="This function is not in use";
+    function BUCKET_MANAGER_ROLE() external pure returns (bytes32) {
+        return "This function is not in use";
+    }
 
-      }
+    function removeFacilitator(address facilitatorAddress) external {
+        value1 = "This function is not in use";
+    }
 
-  
-  function setFacilitatorBucketCapacity(address facilitator, uint128 newCapacity) external{
-            value1="This function is not in use";
-  }
+    function setFacilitatorBucketCapacity(
+        address facilitator,
+        uint128 newCapacity
+    ) external {
+        value1 = "This function is not in use";
+    }
 
-  
-  function getFacilitator(address facilitator) external view returns (Facilitator memory){
-    return _facilitators[facilitator];
-  }
+    function getFacilitator(
+        address facilitator
+    ) external view returns (Facilitator memory) {
+        return _facilitators[facilitator];
+    }
 
- 
-  function getFacilitatorBucket(address facilitator) external view returns (uint256, uint256){
-    return(a,b);
-  }
+    function getFacilitatorBucket(
+        address facilitator
+    ) external view returns (uint256, uint256) {
+        return (a, b);
+    }
 
-  function getFacilitatorsList() external view returns (address[] memory){
-    return arr;
-  }
-
+    function getFacilitatorsList() external view returns (address[] memory) {
+        return arr;
+    }
 }
