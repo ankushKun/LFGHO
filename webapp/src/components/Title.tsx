3f52a74a03779d0cbeaaa19c32a7e7e795cffae7
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-01-20 09:53:22 +0530

    title done

--- webapp/src/components/Title.tsx
+++ webapp/src/components/Title.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { CopyBlock } from 'react-code-blocks';
-
-
+import { CopyBlock, dracula } from "react-code-blocks";
 
 import { ConnectKitButton } from "connectkit";
 import { useState } from "react";
@@ -10,25 +8,28 @@ import { useContractRead } from "wagmi";
 
 export default function Title() {
   const { address, isConnecting, isDisconnected } = useAccount();
- const code= "npm install karma-vouch";
- const language="bash";
- const showLineNumbers=true;
-
+  const code = "npm install karma-vouch";
+  const language = "bash";
+  const showLineNumbers = true;
 
   return (
-    <div className="flex flex-row justify-around	">
-      <div className="w-2/5 h-[30rem]	border-solid border-black	border-4">
-<h1 className="text-9xl	">Karma Vouch</h1>
-<h2 className="text-3xl pt-8	">Elevating Trust in the Blockchain with Karma Vouch - Where DAO Members Validate Genuine Identities</h2>
-     </div>
-     <div className="w-2/5	border-solid border-black	border-4">
-<h1>npm library!</h1>
-<div className="w-72 border-solid border-black	border-4"><CopyBlock
-      text={code}
-      language={language}
-    /></div>
-
-     </div>
+    <div className="w-full h-[35rem] box-border	 bg-slate-600">
+    <div className="flex flex-row justify-around pt-20 pl-14">
+      <div className="w-2/5 h-[30rem] 	">   {/*border-solid border-black	border-4*/ }
+        <h1 className="text-9xl	">Karma Vouch</h1>
+        <h2 className="text-3xl pt-8	">
+          Elevating Trust in the Blockchain with Karma Vouch - Where DAO Members
+          Validate Genuine Identities
+        </h2>
+      </div>
+      <div className="w-2/5	h-[30rem] flex flex-col justify-center items-center gap-7"> {/*border-solid border-black	border-4*/ }
+        <h1 className="text-3xl ">npm library!</h1>
+        <div className="w-72 ">
+          <CopyBlock text={code} language={language} theme={dracula} />
+        </div>
+       
+      </div>
+    </div>
     </div>
   );
 }
