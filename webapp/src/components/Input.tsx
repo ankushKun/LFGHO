1f94f28136f24fb2eaa0cc51d74fdb3042af4578
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-01-20 22:32:27 +0530

    save

--- webapp/src/components/Input.tsx
+++ webapp/src/components/Input.tsx
@@ -1,12 +1,32 @@
 import React from "react";
+import lfgho from "../../../contracts/artifacts/contracts/lfgho.sol/lfgho.json";
+import fs from "fs";
 import { ConnectKitButton } from "connectkit";
 import { useState } from "react";
 import { useAccount } from "wagmi";
 import { useContractRead,useContractWrite } from "wagmi";
 
 export default function Input() {
+  // const addrsJson:any = fs.readFileSync("../../../contracts/smartContractAddress.json");
+  // const addrs = JSON.parse(addrsJson);
+  // console.log(addrs);
+ 
+  // interface ContractWrite {
+  //   address: string,
+  //   abi?: any,
+  //   functionName:string,
+  // }
+
+  console.log(lfgho.abi);
+
   const { address, isConnecting, isDisconnected } = useAccount();
-  
+
+
+  const x  = useContractWrite({
+    address: '0xecb504d39723b0be0e3a9aa33d646642d1051ee1',
+    abi: lfgho.abi,
+    functionName: 'feed',
+  })
 
   return (
     <div>
--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -31,7 +31,7 @@ const config = createConfig(
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <WagmiConfig config={config}>
-      <ConnectKitProvider  >
+      <ConnectKitProvider theme="retro" >
         <App />
       </ConnectKitProvider>
     </WagmiConfig>
