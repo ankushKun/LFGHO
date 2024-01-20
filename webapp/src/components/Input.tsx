5d70ca8b76bacf1f277e1241cbdf9c3aff594db2
AAshu1412 <ashutoshsanjeevmittal@gmail.com>
2024-01-20 23:40:33 +0530

    artifacts upload

--- webapp/src/components/Input.tsx
+++ webapp/src/components/Input.tsx
@@ -1,46 +1,43 @@
 import React from "react";
 import lfgho from "../../../contracts/artifacts/contracts/lfgho.sol/lfgho.json";
-import fs from "fs";
+import {ethers} from "ethers";
 import { ConnectKitButton } from "connectkit";
 import { useState } from "react";
 import { useAccount } from "wagmi";
-import { useContractRead,useContractWrite } from "wagmi";
+import { useContractRead, useContractWrite } from "wagmi";
 
 export default function Input() {
-  // const addrsJson:any = fs.readFileSync("../../../contracts/smartContractAddress.json");
-  // const addrs = JSON.parse(addrsJson);
-  // console.log(addrs);
- 
-  // interface ContractWrite {
-  //   address: string,
-  //   abi?: any,
-  //   functionName:string,
-  // }
-
-  console.log(lfgho.abi);
-
   const { address, isConnecting, isDisconnected } = useAccount();
 
+  // const { data, isLoading, isSuccess, write }  = useContractWrite({
+  //   address: "0x5f6734B2ee6cF2bBf05AAF7aBf03ddCEBE439049",
+  //   abi: lfgho.abi,
+  //   functionName: "mint",
+  // });
 
-  const x  = useContractWrite({
-    address: '0xecb504d39723b0be0e3a9aa33d646642d1051ee1',
-    abi: lfgho.abi,
-    functionName: 'feed',
-  })
+  const connectionAllowingIssuer=async()=>{
+    const provider = new ethers.BrowserProvider((window as any).ethereum);
+        await (window as any).ethereum.request({ method: "eth_requestAccounts" });
+        const signer = await provider.getSigner();
+        const contract = new ethers.Contract(certificateAddress, lfgho.abi, signer);
+        const transaction = await contract.allowingIssuer(_issuerAddress,_organisationName,_companyRegisteredNumber);
+        transaction.wait();
+   }
+  
 
   return (
     <div>
-        <div>
-            <h1>Want to Become DAO Member</h1> <button>Mint</button>
-        </div>
-        <div>
-            <h1>Do you wanna vouch for anyone? DO here </h1>
-            <input className="bg-slate-600"></input> <button>Vouch</button>
-        </div>
-        <div>
-            <h1>Check the Vouches for any address</h1>
-            <input className="bg-slate-600"></input> <button>Vouch</button>
-        </div>
+      <div>
+        <h1>Want to Become DAO Member</h1> <button>Mint</button>
+      </div>
+      <div>
+        <h1>Do you wanna vouch for anyone? DO here </h1>
+        <input className="bg-slate-600"></input> <button>Vouch</button>
+      </div>
+      <div>
+        <h1>Check the Vouches for any address</h1>
+        <input className="bg-slate-600"></input> <button>Vouch</button>
+      </div>
     </div>
   );
 }
