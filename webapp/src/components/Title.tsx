cc3342f2cecc648c839cd3f4da047f4b9a6b39b6
Bhawna Chauhan <bhawnabc2002@gmail.com>
2024-01-21 00:12:54 +0530

    fixed the responsive issue

--- webapp/src/components/Title.tsx
+++ webapp/src/components/Title.tsx
@@ -6,22 +6,26 @@ export default function Title() {
   const language = "bash";
 
   return (
-    <div className="w-full h-[30rem] box-border bg-slate-600">
-      <div className="flex flex-row justify-around pt-20 pl-14">
-        <div className="w-2/5 h-[25rem] "> {/*border-solid border-black	border-4*/}
-          <h1 className="text-9xl">Karma Vouch</h1>
-          <h2 className="text-3xl pt-8">
-            Elevating Trust in the Blockchain with Karma Vouch - Where DAO
-            Members Validate Genuine Identities
-          </h2>
-        </div>
-        <div className="w-2/5 h-[25rem] flex flex-col justify-center items-center gap-7"> {/*border-solid border-black	border-4*/}
-          <h1 className="text-3xl">npm library!</h1>
-          <div className="w-72">
-            <CopyBlock text={code} language={language} theme={dracula} />
-          </div>
-        </div>
+    <div className="w-full h-[45rem] box-border bg-slate-600">
+  <div className="flex flex-col md:flex-row justify-around pt-10 md:pt-20 pl-4 md:pl-14">
+    <div className="w-full md:w-2/5 h-[25rem] md:h-auto mb-8 md:mb-0"> {/*border-solid border-black border-4*/}
+      <h1 className="text-5xl md:text-9xl">Karma Vouch</h1>
+      <h2 className="text-2xl md:text-3xl pt-4 md:pt-8">
+        Elevating Trust in the Blockchain with Karma Vouch - Where DAO Members Validate Genuine Identities
+      </h2>
+    </div>
+    <div className="w-full md:w-2/5 h-[25rem] md:h-auto flex flex-col justify-center items-center gap-7"> {/*border-solid border-black border-4*/}
+      <h1 className="text-2xl md:text-3xl mb-1">npm library!</h1>
+      <div className="w-full md:w-72">
+        {/* Assuming you want the code block to be full width on small screens and 72 on medium and large screens */}
+        <CopyBlock text={code} language={language} theme={dracula} />
       </div>
     </div>
+  </div>
+</div>
+
+ 
+    
+
   );
 }
